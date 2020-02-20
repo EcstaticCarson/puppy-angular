07e361192c7e7f7b613fb142194d6b920660895f
Code <code@COCTL-41297.local>
2020-02-19 16:01:23 -0800

    finished app-routi. Finish Head and Footer tonight

--- puppy-me/src/app/app-routing.module.ts
+++ puppy-me/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './folder/main/main.component';
+import { AboutComponent } from './folder/about/about.component';
+import { MenuComponent } from './folder/menu/menu.component';
+import { CateringComponent } from './folder/catering/catering.component';
+import { PhotosComponent } from './folder/photos/photos.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'main', component: MainComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: 'catering', component: CateringComponent},
+  {path: 'photos', component: PhotosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
